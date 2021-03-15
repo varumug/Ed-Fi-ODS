70adb4602e06996f4a736a17030f8c967263252d
Jim McKay <james.mckay@instructure.com>
2021-03-15 12:15:27 -0500

    fix broken tests

--- Application/EdFi.Ods.Api/Helpers/AssemblyLoaderHelper.cs
+++ Application/EdFi.Ods.Api/Helpers/AssemblyLoaderHelper.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -108,7 +108,8 @@ namespace EdFi.Ods.Api.Helpers
                    && !assemblyName.StartsWithIgnoreCase("System.")
                    && !assemblyName.StartsWithIgnoreCase("Newtonsoft.")
                    && assemblyName != "netstandard"
-                   && !assemblyName.StartsWithIgnoreCase("Autofac");
+                   && !assemblyName.StartsWithIgnoreCase("Autofac")
+                   && !assemblyName.StartsWithIgnoreCase("nunit");
         }
 
         public static IEnumerable<string> FindPluginAssemblies(string pluginFolder, bool includeFramework = false)
